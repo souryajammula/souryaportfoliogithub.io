62336beeb37bc256c7e5455b05fccc2432910331
Sourya Jammula <souryaprateek2000@gmail.com>
2023-09-03 18:40:46 -0400

    fix error:redirection

--- js/custom.js
+++ js/custom.js
@@ -116,7 +116,7 @@ form.addEventListener('submit', e => {
   fetch(scriptURL, { method: 'POST', body: new FormData(form), mode: 'no-cors' })
     .then(response => {
       alert("Your message was sent successfully! I will be in touch as soon as I can.");
-      location.assign("https://souryajammula.github.io/souryaportfolio.github.io/");
+      location.assign("https://souryajammula.github.io/souryaportfoliogithub.io/");
     })
     .catch(error => console.error('Error!', error.message));
 });
